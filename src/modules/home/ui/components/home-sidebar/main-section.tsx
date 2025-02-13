60315a6e13c2c5860f5df8e3c33ad4bed3d68205
LukaD45 <luka.dusko44@gmail.com>
2025-02-13 21:46:06 +0100

    created main section component

---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+import Link from "next/link";
+
+const items = [
+  { title: "Home", icon: HomeIcon, url: "/" },
+  {
+    title: "Subscriptions",
+    icon: PlaySquareIcon,
+    url: "/feed/subscriptions",
+    auth: true,
+  },
+  { title: "Trending", icon: FlameIcon, url: "/feed/trending" },
+];
+
+export const MainSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} //todo: change to look at the current pathname
+                onClick={() => {}} //todo: change to navigate to the item's url
+              >
+                <Link href={item.url} className="flex items-center gap-4 ">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
