36b2def56d88b1847c5002fe3cd7952535ae06fe
LukaD45 <luka.dusko44@gmail.com>
2025-02-18 14:10:40 +0100

    create home-view & categorie-section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,14 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
 export const dynamic = "force-dynamic";
 
 interface PageProps {
-  searchParams: Promise<[categoryId?: string]>;
+  searchParams: Promise<{ categoryId?: string }>;
 }
 
 const Page = async ({ searchParams }: PageProps) => {
-  const [categoryId] = await searchParams;
+  const { categoryId } = await searchParams;
 
   void trpc.categories.getMany.prefetch();
 
