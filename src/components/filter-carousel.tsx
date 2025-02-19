6f6331702f7a8a1c608fa1423dbc0df6c8fb28fe
LukaD45 <luka.dusko44@gmail.com>
2025-02-19 11:28:23 +0100

    create carosusel api for hidding fade on first itme and last item

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -10,6 +10,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "./ui/carousel";
+import { useEffect, useState } from "react";
 
 interface FilterCarouselProps {
   value?: string | null;
@@ -27,16 +28,31 @@ export const FilterCarousel = ({
   onSelect,
   data,
 }: FilterCarouselProps) => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
   return (
     <div className="relative w-full">
       {/*Left fade */}
       <div
         className={cn(
           "absolute left-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-r from-white to-transparent pointer-events-none",
-          false && "hidden"
+          current === 1 && "hidden"
         )}
       />
       <Carousel
+        setApi={setApi}
         opts={{ align: "start", dragFree: true }}
         className="w-full px-12"
       >
@@ -67,7 +83,7 @@ export const FilterCarousel = ({
       <div
         className={cn(
           "absolute right-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-l from-white to-transparent pointer-events-none",
-          false && "hidden"
+          current === count && "hidden"
         )}
       />
     </div>
