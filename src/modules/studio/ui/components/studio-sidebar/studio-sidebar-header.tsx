94695ebd294fc950804e31bf755f90bc86a890ac
LukaD45 <luka.dusko44@gmail.com>
2025-02-20 09:19:46 +0100

    create skeleton for studio sidebar header

--- src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -3,11 +3,21 @@ import { useUser } from "@clerk/nextjs";
 
 import { SidebarHeader } from "@/components/ui/sidebar";
 import UserAvatar from "@/components/user-avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const StudioSidebarHeader = () => {
   const { user } = useUser();
 
-  if (!user) return null;
+  if (!user)
+    return (
+      <SidebarHeader className="flex items-center justify-center pb-4">
+        <Skeleton className="size-[112px] rounded-full" />
+        <div className="flex flex-col items-center mt-2 gap-y-1">
+          <Skeleton className="h-4 w-[80px]" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </SidebarHeader>
+    );
 
   return (
     <SidebarHeader className="flex items-center justify-center pb-4">
@@ -18,7 +28,7 @@ const StudioSidebarHeader = () => {
           className="size-[112px] hover:opacity-80 transition-opacity"
         />
       </Link>
-      <div className="flex flex-col items-center mt-2">
+      <div className="flex flex-col items-center mt-2 gap-y-1">
         <p className="text-xs font-medium">Your profile</p>
         <p className="text-xs text-muted-foreground">{user.fullName}</p>
       </div>
