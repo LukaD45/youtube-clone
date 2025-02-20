c1e47db43b98890bda30ced05eaf76d48bc35387
LukaD45 <luka.dusko44@gmail.com>
2025-02-20 11:39:58 +0100

    expand studio protectedProcedure

--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,11 +1,31 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
 
 export const studioRouter = createTRPCRouter({
-  getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(videos);
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { cursor, limit } = input;
+      const { id: userId } = ctx.user;
 
-    return data;
-  }),
+      const data = await db
+        .select()
+        .from(videos)
+        .where(eq(videos.userId, userId));
+
+      return data;
+    }),
 });
