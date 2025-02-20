f4ca085bf8be94f2534098510a9b74bd4e8472ac
LukaD45 <luka.dusko44@gmail.com>
2025-02-20 10:41:35 +0100

    create studio procedure and implement inside tRPC router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,11 @@
-import { z } from "zod";
+import { studioRouter } from "@/modules/studio/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+
 import { createTRPCRouter } from "../init";
 
-export const appRouter = createTRPCRouter({ categories: categoriesRouter });
+export const appRouter = createTRPCRouter({
+  categories: categoriesRouter,
+  studio: studioRouter,
+});
 // export type definition of API
 export type AppRouter = typeof appRouter;
