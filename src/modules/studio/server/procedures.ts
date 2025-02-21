85707b4b176e5d9f705d46fcb5e1505f31d55267
LukaD45 <luka.dusko44@gmail.com>
2025-02-21 12:52:37 +0100

    studio procedure finished

--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { eq } from "drizzle-orm";
+import { eq, and, or, lt, desc } from "drizzle-orm";
 import { z } from "zod";
 
 export const studioRouter = createTRPCRouter({
@@ -24,8 +24,39 @@ export const studioRouter = createTRPCRouter({
       const data = await db
         .select()
         .from(videos)
-        .where(eq(videos.userId, userId));
+        .where(
+          and(
+            eq(videos.userId, userId),
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(videos.updatedAt, cursor.updatedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        //Add 1 to the limit to check if there is more data
+        .limit(limit + 1);
 
-      return data;
+      const hasMore = data.length > limit;
+
+      //Remove the last item if there is more data
+      const items = hasMore ? data.slice(0, -1) : data;
+      //Set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+      return {
+        items,
+        nextCursor,
+      };
     }),
 });
