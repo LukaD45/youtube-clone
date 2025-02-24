008d07d198e619747093abef00bd196d923f9245
LukaD45 <luka.dusko44@gmail.com>
2025-02-24 11:33:59 +0100

    add suspense to videosSection component

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -2,8 +2,20 @@
 
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const VideosSection = () => {
+export const VideosSection = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
   const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     { limit: DEFAULT_LIMIT },
     {
