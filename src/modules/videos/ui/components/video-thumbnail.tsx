25ef163ff090859047254f2a750e4065d669f6ca
LukaD45 <luka.dusko44@gmail.com>
2025-02-25 19:45:35 +0100

    able to see video duration

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -17,13 +17,13 @@ export const VideoThumbnail = ({
       <div className="relative w-full overflow-hidden rounded-xl aspect-video">
         <Image
           src={imageUrl || "/placeholder.svg"}
-          alt="Thumbnail"
+          alt={title}
           fill
           className="size-full object-cover group-hover:opacity-0"
         />
         <Image
           src={previewUrl || "/placeholder.svg"}
-          alt="Thumbnail"
+          alt={title}
           fill
           className="size-full object-cover opacity-0 group-hover:opacity-100"
         />
