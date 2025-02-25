741c36e2367278d38a55ec3ed047187fc27ca0c6
LukaD45 <luka.dusko44@gmail.com>
2025-02-25 19:42:52 +0100

    add on hover effect to switch from imageUrl to preview url

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -12,14 +12,20 @@ export const VideoThumbnail = ({
   previewUrl,
 }: VideoThumbnailProps) => {
   return (
-    <div className="relative">
+    <div className="relative group">
       {/*Thumbnail wrapper */}
       <div className="relative w-full overflow-hidden rounded-xl aspect-video">
         <Image
           src={imageUrl || "/placeholder.svg"}
           alt="Thumbnail"
           fill
-          className="size-full object-cover"
+          className="size-full object-cover group-hover:opacity-0"
+        />
+        <Image
+          src={previewUrl || "/placeholder.svg"}
+          alt="Thumbnail"
+          fill
+          className="size-full object-cover opacity-0 group-hover:opacity-100"
         />
       </div>
 
