ed4729042ffab60a35a6d53e7895b33623eee0cf
LukaD45 <luka.dusko44@gmail.com>
2025-02-26 20:57:31 +0100

    create dynamic videoId page

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -1,5 +1,20 @@
-const VideoPage = () => {
-  return <div>VideoPage</div>;
+import { HydrateClient, trpc } from "@/trpc/server";
+
+export const dynamic = "force-dynamic";
+interface VideoPageProps {
+  params: Promise<{ videoId: string }>;
+}
+
+const VideoPage = async ({ params }: VideoPageProps) => {
+  const { videoId } = await params;
+
+  void trpc.studio.getOne({ id: videoId });
+
+  return (
+    <HydrateClient>
+      <VideoView videoId={videoId} />
+    </HydrateClient>
+  );
 };
 
 export default VideoPage;
