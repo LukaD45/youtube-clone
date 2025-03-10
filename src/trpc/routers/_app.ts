856c2872fd6e98e8b8b215fa65b7716817d92fe3
LukaD45 <luka.dusko44@gmail.com>
2025-03-10 15:15:15 +0100

    implement videoReactions functionality

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { videosRouter } from "@/modules/videos/server/procedures";
 
 import { createTRPCRouter } from "../init";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
   videos: videosRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
