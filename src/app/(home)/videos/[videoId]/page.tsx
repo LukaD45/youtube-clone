c4b4ae2863d2475aa05a1b07e1bc73bcea7ac18e
LukaD45 <luka.dusko44@gmail.com>
2025-03-10 11:00:05 +0100

    create videoViews inside schema and videoViewRelations

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -1,4 +1,4 @@
-import { VideoView } from "@/modules/videos/ui/components/video-view";
+import { VideoView } from "@/modules/videos/ui/views/video-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
 interface PageProps {
