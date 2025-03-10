80a716cd7a6ac1fa8f67db310cb638fd44121402
LukaD45 <luka.dusko44@gmail.com>
2025-03-10 12:52:24 +0100

    update query for getOne existingVideo procedure

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 
 import { createTRPCRouter } from "../init";
+import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
   videos: videosRouter,
+  videoViews: videoViewsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
