a4978294e67cb227b7190edf83e736a28f77efb2
LukaD45 <luka.dusko44@gmail.com>
2025-03-13 20:39:23 +0100

    prefetch comments on videoId page

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -9,6 +9,8 @@ const Page = async ({ params }: PageProps) => {
   const { videoId } = await params;
 
   void trpc.videos.getOne.prefetch({ id: videoId });
+  //TODO: don't fotget to change to 'prefetchInfinite'
+  void trpc.comments.getMany.prefetch({ videoId: videoId });
 
   return (
     <HydrateClient>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -6,7 +6,7 @@ import { createTRPCRouter } from "../init";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
-
+import { commentsRouter } from "@/modules/comments/server/procedures";
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
@@ -14,6 +14,7 @@ export const appRouter = createTRPCRouter({
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
   subscriptions: subscriptionsRouter,
+  comments: commentsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
