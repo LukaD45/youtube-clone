9f01cdaf372c68cd99af17e4b9248593602fdee6
LukaD45 <luka.dusko44@gmail.com>
2025-03-13 20:46:22 +0100

    create comments suspense for manipulating loading state

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface CommentsSectionProps {
   videoId: string;
 }
 
 export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <CommentsSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
   const [comments] = trpc.comments.getMany.useSuspenseQuery({
     videoId,
   });
