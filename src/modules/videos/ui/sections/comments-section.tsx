937705c6d8703ae6b898a459b708e3f70962bb04
LukaD45 <luka.dusko44@gmail.com>
2025-03-13 20:42:54 +0100

    display stringified comments

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,3 +1,14 @@
-export const CommentsSection = () => {
-  return <div>Comments Section</div>;
+"use client";
+
+import { trpc } from "@/trpc/client";
+
+interface CommentsSectionProps {
+  videoId: string;
+}
+
+export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
+  const [comments] = trpc.comments.getMany.useSuspenseQuery({
+    videoId,
+  });
+  return <div>{JSON.stringify(comments)}</div>;
 };
