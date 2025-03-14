f2ac0b8e0276fe4b5cfa197050afd12e02965b0e
LukaD45 <luka.dusko44@gmail.com>
2025-03-14 19:20:35 +0100

    display full details of comment

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { CommentsGetManyOutput } from "../../types";
 import UserAvatar from "@/components/user-avatar";
+import { formatDistanceToNow } from "date-fns";
 
 interface CommentItemProps {
   comment: CommentsGetManyOutput[number];
@@ -17,6 +18,21 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
             name={comment.user.name}
           />
         </Link>
+        <div className="flex-1 min-w-0">
+          <Link href={`/users/${comment.userId}`}>
+            <div className="flex items-center mb-0.5 gap-2">
+              <span className="font-medium text-sm pb-0.5">
+                {comment.user.name}
+              </span>
+              <span className="text-xs text-muted-foreground">
+                {formatDistanceToNow(comment.createdAt, {
+                  addSuffix: true,
+                })}
+              </span>
+            </div>
+          </Link>
+          <p className="text-sm">{comment.value}</p>
+        </div>
       </div>
     </div>
   );
