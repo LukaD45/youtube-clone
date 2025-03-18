f27e9250b06e21e586109b14b2261d41940ad3ca
LukaD45 <luka.dusko44@gmail.com>
2025-03-18 22:10:01 +0100

    create comment-reactions module and create procedure for like or dislike comment

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -7,6 +7,8 @@ import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
 import { commentsRouter } from "@/modules/comments/server/procedures";
+import { commentReactionsRouter } from "@/modules/comment-reactions/server/procedures";
+
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
@@ -15,6 +17,7 @@ export const appRouter = createTRPCRouter({
   videoReactions: videoReactionsRouter,
   subscriptions: subscriptionsRouter,
   comments: commentsRouter,
+  commentReactions: commentReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
