12e6b56379036bdb1b38693c788376c07996140f
LukaD45 <luka.dusko44@gmail.com>
2025-03-21 15:13:43 +0100

    display suggestions video under/alongside videoPlayer

--- src/modules/videos/ui/components/video-row-card.tsx
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -16,3 +16,51 @@ import { UserAvatar } from "@/components/user-avatar";
 import { VideoMenu } from "./video-menu";
 import { VideoThumbnail } from "./video-thumbnail";
 import { VideoGetManyOutput } from "../../types";
+
+const videoRowCardVariants = cva("group flex min-w-0 ", {
+  variants: {
+    size: {
+      default: "gap-4",
+      compact: "gap-2",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+const thumbnailVariants = cva("relatvie flex-none", {
+  variants: {
+    size: {
+      default: "w-[38%]",
+      compact: "w-[168px]",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+interface VideoRowCardProps extends VariantProps<typeof videoRowCardVariants> {
+  data: VideoGetManyOutput["items"][number];
+  onRemove?: () => void;
+}
+
+export const VideoRowCardSkeleton = () => {
+  return <div className="flex min-w-0 flex-col gap-2">skeleton</div>;
+};
+
+export const VideoRowCard = ({ data, size, onRemove }: VideoRowCardProps) => {
+  return (
+    <div className={videoRowCardVariants({ size })}>
+      <Link href={`/videos/${data.id}`} className={thumbnailVariants({ size })}>
+        <VideoThumbnail
+          imageUrl={data.thumbnailUrl}
+          previewUrl={data.previewUrl}
+          title={data.title}
+          duration={data.duration}
+        />
+      </Link>
+    </div>
+  );
+};
--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -2,6 +2,7 @@
 
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { VideoRowCard } from "../components/video-row-card";
 
 interface SuggestionsSectionProps {
   videoId: string;
@@ -18,5 +19,13 @@ export const SuggestionsSection = ({ videoId }: SuggestionsSectionProps) => {
     }
   );
 
-  return <div>{JSON.stringify(suggestions)}</div>;
+  return (
+    <div>
+      {suggestions.pages.flatMap((page) =>
+        page.items.map((video) => (
+          <VideoRowCard key={video.id} data={video} size="compact" />
+        ))
+      )}
+    </div>
+  );
 };
